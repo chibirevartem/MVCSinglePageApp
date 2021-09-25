79bef0013be22210cb3ce6ea2fef51328e6f54fe
Chibirev Artem <chibirevas@rubezh.ru>
2021-09-25 19:35:27 +0300

    HOM-2 Создан пустой шаблон приложения ASP.NET MVC C#, добавлены сущности

---
+++ WebServiceRepo/WebService.FakeDAL/Models/Entities/Organisation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebService.FakeDAL.Models.Interfaces;
+
+namespace WebService.FakeDAL.Models.Entities
+{
+    /// <summary>
+    /// Модель организации
+    /// </summary>
+    public class Organisation : Interfaces.IOrganisation
+    {
+        private string _name;
+        private long _taxId;
+        private long _registrationReasonCode;
+
+        public string Name { get => _name; set => _name = value; }
+        public long TaxId { get => _taxId; set => _taxId = value; }
+        public long RegistrationReasonCode { get => _registrationReasonCode; set => _registrationReasonCode = value; }
+
+    }
+}
---
+++ WebServiceRepo/WebService.FakeDAL/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebService.FakeDAL.Models.Interfaces;
+
+namespace WebService.FakeDAL.Repository.Interfaces
+{
+    public interface IRepository<T>
+        where T : IOrganisation
+    {
+        void Create(T organisation);
+        T Read(int taxId);
+        void Update(T organisation);
+        void Delete(T organisation);
+        
+        IEnumerable<T> GetAll();
+    }
+}
