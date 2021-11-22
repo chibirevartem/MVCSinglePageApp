47fee7d266133cd57d65841b83e65dee8be30db7
Chibirev Artem <chibirevas@rubezh.ru>
2021-11-22 13:40:01 +0400

    First Commit

--- WebServiceRepo/WebService/Global.asax.cs
+++ WebServiceRepo/WebService/Global.asax.cs
@@ -17,7 +17,9 @@ namespace WebService
             // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            Console.WriteLine("First commit");
+            Console.WriteLine("Third commit");
         }
     }
 }
